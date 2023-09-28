cc29e51a3acbb522f5d8bc890d30ae32d0b6b5ed
tcson62 <tcson62@gmail.com>
2023-09-27 21:20:32 -0600

    Add results mapf 1010

--- mapf/88-33/1error/run-m33.sh
+++ mapf/88-33/1error/run-m33.sh
@@ -1,6 +1,6 @@
 
-swipl -l ../../progs/env.pl -g "load_config('conf.pl'), halt." > e_run.lp & 
-swipl -l ../../progs/agent.pl -g "load_agent_config('r1.lp'), halt." > 1_run.lp & 
-swipl -l ../../progs/agent.pl -g "load_agent_config('r2.lp'), halt." > 2_run.lp &
-swipl -l ../../progs/agent.pl -g "load_agent_config('r3.lp'), halt." > 3_run.lp &
+swipl -l ../../../progs/env.pl -g "load_config('conf.pl'), halt." > e_run.lp & 
+swipl -l ../../../progs/agent.pl -g "load_agent_config('r1.lp'), halt." > 1_run.lp & 
+swipl -l ../../../progs/agent.pl -g "load_agent_config('r2.lp'), halt." > 2_run.lp &
+swipl -l ../../../progs/agent.pl -g "load_agent_config('r3.lp'), halt." > 3_run.lp &
 
